b14a52eaa573da556d21684fee8a86f37b993fc2
Renato Rosseto <renato.rosseto@totvs.com.br>
2024-06-24 20:45:25 -0300

    creating product model

---
+++ src/models/Product.ts
@@ -0,0 +1,19 @@
+export interface Price {
+  currency: string;
+  amount: number;
+  decimals: number;
+}
+
+export interface Product {
+  id: string;
+  title: string;
+  price: Price;
+  picture_url: string;
+  condition: string;
+  free_shipping: boolean;
+}
+
+export interface ProductDetails extends Product {
+  sold_qty: number;
+  description: string;
+}
