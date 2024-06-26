5dc70d4046298b065618b331021c76a193def12d
Renato Rosseto <renato.rosseto@totvs.com.br>
2024-06-26 18:18:54 -0300

    adding cors, allowing all origins

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 
 import productsRoutes from './routes/productsRoutes'
 import productDetailsRoutes from './routes/productDetailsRoutes'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/products', productsRoutes);
 app.use('/api/productDetails', productDetailsRoutes);
